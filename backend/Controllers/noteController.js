4d19e259436f0bcabfba8602db704a79d700c64c
Lynn <lynn.yingnanliu@gmail.com>
2022-04-05 14:27:30 +0800

    controllers setup

--- backend/Controllers/noteController.js
+++ backend/Controllers/noteController.js
@@ -1,37 +1,45 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc   Get notes
 // @route GET /api/goals
 // @access Private
-const getNotes = (req, res) => {
+const getNotes = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: "get successed",
   });
-};
+});
 
 // @desc   Set notes
 // @route POST /api/goals
 // @access Privat
-const setNote = (req, res) => {
+const setNote = asyncHandler(async (req, res) => {
+  console.log(req.body);
+
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
   res.status(200).json({
     message: "post successed",
   });
-};
+});
 
 // @desc   update notes
 // @route PUT /api/goals/:id
 // @access Private
-const updateNote = (req, res) => {
+const updateNote = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `put successed id: ${req.params.id}`,
   });
-};
+});
 // @desc   delete notes
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteNote = (req, res) => {
+const deleteNote = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `delete successed id: ${req.params.id}`,
   });
-};
+});
 
 module.exports = {
   getNotes,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config(); //环境变量 allow a .env file with our enviroment variables in it
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json()); //解析req.body
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/notes", require("./Routes/noteRoutes"));
+app.use(errorHandler); //重写express的errhandler
 
 app.listen(port, () => {
   console.log(`Server start on port ${port}`);
