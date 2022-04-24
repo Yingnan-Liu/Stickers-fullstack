684c497b85a5bc181370e9e1ae3448aa6e071955
Lynn <lynn.yingnanliu@gmail.com>
2022-04-24 16:21:37 +0800

    basic routes setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Home from "./pages/Home/Home";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div>This is SignUp Page</div>;
+};
+
+export default SignUp;
