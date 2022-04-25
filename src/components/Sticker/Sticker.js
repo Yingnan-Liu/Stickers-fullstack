b13545d7af493fd29237321897f2df825d0f7ea2
Lynn <lynn.yingnanliu@gmail.com>
2022-04-25 13:28:54 +0800

    signin page

--- src/components/Sticker/Sticker.js
+++ src/components/Sticker/Sticker.js
@@ -1,13 +1,37 @@
-import React from "react";
-import { Paper, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import { Paper, TextArea, IconButton, Chip } from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AssignmentTurnedInIcon from "@material-ui/icons/AssignmentTurnedIn";
 import "./style.scss";
 
-const Sticker = () => {
+const Sticker = ({ content }) => {
+  const [text, setText] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+  const handleEdit = () => {
+    setIsEdit(true);
+  };
+  const handleSave = () => {
+    setIsEdit(false);
+  };
   return (
     <div className="sticker">
-      <Paper className="paper" elevation={3}>
-        1232435
-      </Paper>
+      <div className="paper">
+        <div className="content-area">{text}</div>
+        <div className="paper-btns">
+          <Chip className="time-staple" label="2020/4/25" />
+          <IconButton aria-label="edit">
+            {isEdit ? (
+              <AssignmentTurnedInIcon onClick={handleSave} />
+            ) : (
+              <EditIcon onClick={handleEdit} />
+            )}
+          </IconButton>
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/StickerList/StickerList.js
+++ src/components/StickerList/StickerList.js
@@ -6,9 +6,6 @@ const Stickerlist = () => {
   return (
     <div className="sticker-list">
       <Sticker />
-      <Sticker />
-      <Sticker />
-      <Sticker />
     </div>
   );
 };
